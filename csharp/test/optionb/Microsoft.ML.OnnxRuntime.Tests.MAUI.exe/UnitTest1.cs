c74613021ddb6e6c1b283ddb0da7626ff43dab46
Scott McKay <Scott.McKay@microsoft.com>
2024-07-26 18:07:03 +1000

    WIP - not sure if the setup will work. Has MAUI lib where intension was to pull in the ORT stuff at that level and a MAUI xunit exe.
    But it may require XHarness to run those tests on device which is adding more overhead to the update.
    
    https://github.com/mattleibow/DeviceRunners/wiki/CLI-Device-Runner-for-Android-using-XHarness
    
    Trying upgrading the existing Droid and iOS projects instead.

---
+++ csharp/test/optionb/Microsoft.ML.OnnxRuntime.Tests.MAUI.exe/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Microsoft.ML.OnnxRuntime.Tests.MAUI.exe
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ csharp/test/optionb/Microsoft.ML.OnnxRuntime.Tests.MAUI/Platforms/MacCatalyst/InferenceTest.maccatalyst.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Microsoft.ML.OnnxRuntime.Tests
+{
+    public partial class InferenceTest
+    {
+        [Fact(DisplayName = "TestPlatformSessionOptions")]
+        public void TestPlatformSessionOptions()
+        {
+            var opt = new SessionOptions();
+            opt.AppendExecutionProvider_CoreML(CoreMLFlags.COREML_FLAG_USE_CPU_ONLY);
+        }
+    }
+}
