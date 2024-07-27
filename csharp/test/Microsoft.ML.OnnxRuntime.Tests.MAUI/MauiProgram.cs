a78383c0031bf8b62fd7b42fdbec3c930370b7fc
Scott McKay <Scott.McKay@microsoft.com>
2024-07-27 10:50:39 +1000

    Add MAUI test app. Need to start adding tests.

---
+++ csharp/test/Microsoft.ML.OnnxRuntime.Tests.MAUI/MauiProgram.cs
@@ -0,0 +1,47 @@
+﻿#define MODE_XHARNESS
+
+using DeviceRunners.VisualRunners;
+using Microsoft.Extensions.Logging;
+
+#if MODE_XHARNESS
+using DeviceRunners.XHarness;
+#endif
+
+namespace Microsoft.ML.OnnxRuntime.Tests.MAUI;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            // .ConfigureUITesting()
+#if MODE_XHARNESS
+            .UseXHarnessTestRunner(conf => conf
+                .AddTestAssembly(typeof(MauiProgram).Assembly)
+                .AddTestAssemblies(typeof(Microsoft.ML.OnnxRuntime.Tests.MAUI.UnitTests).Assembly)
+                .AddXunit())
+#endif
+            .UseVisualTestRunner(conf => conf
+//#if MODE_NON_INTERACTIVE_VISUAL
+//                .EnableAutoStart(true)
+//                .AddTcpResultChannel(new TcpResultChannelOptions
+//                {
+//                    HostNames = ["localhost", "10.0.2.2"],
+//                    Port = 16384,
+//                    Formatter = new TextResultChannelFormatter(),
+//                    Required = false
+//                })
+//#endif
+                .AddConsoleResultChannel()
+                .AddTestAssembly(typeof(MauiProgram).Assembly)
+                .AddTestAssemblies(typeof(Microsoft.ML.OnnxRuntime.Tests.MAUI.UnitTests).Assembly)
+                .AddXunit());
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
