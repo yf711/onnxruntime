ffe549ad43f22d8475dd24599f16bc706af45a81
Yi Zhang <your@email.com>
2024-08-06 09:55:55 +0800

    check

--- tools/ci_build/get_docker_image.py
+++ tools/ci_build/get_docker_image.py
@@ -98,20 +98,25 @@ def main():
         )
 
     if use_container_registry:
-        run(args.docker_path, "buildx", "create", "--driver=docker-container", "--name=container_builder")
+        run(args.docker_path,
+            "buildx",
+            "create",
+            "--driver=docker-container",
+            "--name=container_builder"
+            )
         run(
             args.docker_path,
             "--log-level",
             "error",
             "build",
-            "--load",
-            "--tag",
-            full_image_name,
-            "--cache-from=type=registry,ref=" + f"{args.container_registry}.azurecr.io/{args.repository}:cache",
-            "--cache-to=type=registry,mode=max,ref=" + f"{args.container_registry}.azurecr.io/{args.repository}:cache",
+            "--cache-from=type=registry,ref=" + full_image_name,
+            "--cache-to=type=registry,mode=max,ref=" + full_image_name,
             "--builder",
             "container_builder",
             *shlex.split(args.docker_build_args),
+            "--tag",
+            full_image_name,
+            "--load",
             "-f",
             args.dockerfile,
             args.context,
